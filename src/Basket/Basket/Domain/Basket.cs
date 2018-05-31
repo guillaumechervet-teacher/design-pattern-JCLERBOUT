7d3c8ad1ca92f54d62f3604b96fca1682fbfc76a
MartinTop <martin.top.mt@gmail.com>
2018-05-31 12:22:00 +0200

    Ceci est un commit

---
+++ src/Basket/Basket/Domain/Basket.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Domain
+{
+    public class Basket
+    {
+        private IList<BasketLine> LLineArticles  { get; set; }
+
+        public Basket(IList<BasketLine> lineArticles)
+        {
+            this.LLineArticles = lineArticles;
+        }
+    }
+}
---
+++ src/Basket/Basket/Infrastructure/BasketService.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Domain;
+using Basket = Domain.Basket;
+
+namespace Basket.Infrastructure
+{
+    public class BasketService
+    {
+        public Domain.Basket GetBasket(IList<BasketLineArticle> basketLineArticles)
+        {
+            Domain.Basket basket = new Domain.Basket();
+            basket.LineArticles = basketLineArticles;
+
+            return basket;
+        }
+    }
+}
